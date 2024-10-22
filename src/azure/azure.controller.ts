469813786a850874dc1948cb2bd05011426238d7
vamsi2106 <saivamsi1121@gmail.com>
2024-10-22 18:06:13 +0530

    added auth for validate-email endpoint

--- src/azure/azure.controller.ts
+++ src/azure/azure.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { AzureService } from './azure.service';
+import { JwtAuthGuard } from 'src/modules/auth/jwt-auth-guard.guard';
+import { RoleGuard } from 'src/modules/auth/role.guard';
+import { Roles } from 'src/modules/auth/role.decorator';
+import { Role } from 'src/core/enums/roles.enum';
 
 @Controller('azure')
 export class AzureController {
   constructor(private readonly azureService: AzureService) {}
 
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Roles(Role.admin)    
   @Post('validate-email')
   async validateEmail(@Body('email') email: string) {
     return await this.azureService.validateEmail(email);
